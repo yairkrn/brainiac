7dafde8402f7175c21ecb968d40398353ca00fb8
Yair Karin <yair.karin@watchful.ai>
2019-12-27 19:14:43 +0200

    Fixed bytes to rgb conversion.

--- brainiac/parsers/color_image.py
+++ brainiac/parsers/color_image.py
@@ -4,13 +4,17 @@ from PIL import Image
 class ColorImageParser:
     field = 'color_image'
 
-    @staticmethod
-    def __chunker(seq, size):
+    @classmethod
+    def _chunker(cls, seq, size):
         return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+    @classmethod
+    def _bytes_to_rgb(cls, bytes):
+        return [tuple(rgb) for rgb in cls._chunker(bytes, 3)]
 
     def parse(self, context, snapshot):
         width = snapshot.color_image.w
         height = snapshot.color_image.h
         image = Image.new('RGB', (width, height))
-        image.putdata([tuple(rgb) for rgb in self.__chunker(snapshot.color_image.colors, 3)])
+        image.putdata(self._bytes_to_rgb(snapshot.color_image.colors))
         image.save(context.directory / 'color_image.png')
